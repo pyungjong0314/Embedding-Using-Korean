2ff7ad0120c305314304cb3e4fefa1c2551c553f
pyungjong0314 <sonopj@gmail.com>
2024-05-04 16:41:38 +0900

    rename

--- test_soynlp.py
+++ test_soynlp.py
@@ -2,7 +2,7 @@ import math
 from soynlp.word import WordExtractor
 from soynlp.tokenizer import LTokenizer
 
-model_fname = "C:/Users/강평종/Desktop/딥러닝/soyword.model"
+model_fname = "C:/Users/강평종/Desktop/Embedding-Using-Korean/soyword.model"
 
 word_extractor = WordExtractor(min_frequency=100, min_cohesion_forward=0.05, min_right_branching_entropy=0.0)
 word_extractor.load(model_fname)
--- train_soynlp.py
+++ train_soynlp.py
@@ -1,7 +1,7 @@
 from soynlp.word import WordExtractor
 
-corpus_fname = "C:/Users/강평종/Desktop/딥러닝/case1000.txt"
-model_fname = "C:/Users/강평종/Desktop/딥러닝/soyword.model"
+corpus_fname = "C:/Users/강평종/Desktop/Embedding-Using-Korean/case1000.txt"
+model_fname = "C:/Users/강평종/Desktop/Embedding-Using-Korean/soyword.model"
 
 sentences = [sent.strip() for sent in open(corpus_fname, 'r', encoding='utf-8').readlines()]
 # 최소한 100번 등장한 단어만 포함
